51d48cd461da249f39a4f03a2d6130bd3cbd561e
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-07-02 13:59:14 +0300

    added requirements in setup.cfg

--- src/phone/server.py
+++ src/phone/server.py
@@ -6,7 +6,7 @@ from phone import phone
 app = FastAPI()
 
 
-@app.post("/unify_phone_from_json/")
+@app.post("/unify_phone_from_json")
 async def format_phone(request: Request) -> str:
     number = await request.json()
     return phone(number['phone'])
