af571ae584e7beae444731ac864ac5f9e9c44702
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-07-04 06:54:32 +0000

    solved all the tasks

--- src/phone/server.py
+++ src/phone/server.py
@@ -1,7 +1,8 @@
-from fastapi import Request
+from fastapi import Cookie
 from fastapi import FastAPI
-from fastapi import Response
 from fastapi import Form
+from fastapi import Request
+from fastapi import Response
 
 from phone import get_formatted_phone
 
@@ -14,7 +15,14 @@ async def format_phone(request: Request) -> Response:
     number = await request.json()
     return Response(get_formatted_phone(number['phone']), media_type="text/html")
 
-
 @app.post("/unify_phone_from_form")
 async def format_phone_form(phone: str = Form()) -> Response:
+    return Response(get_formatted_phone(phone), media_type="text/html")
+
+@app.get("/unify_phone_from_query")
+async def format_phone_query(phone: str) -> Response:
+    return Response(get_formatted_phone(phone), media_type="text/html")
+
+@app.get("/unify_phone_from_cookies")
+async def format_phone_cookie(phone: str = Cookie()) -> Response:
     return Response(get_formatted_phone(phone), media_type="text/html")
