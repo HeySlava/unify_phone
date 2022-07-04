97ae37238ff90f1493de06c571853b4e9a8ef636
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-07-04 14:40:25 +0300

    flake8 formatting

--- src/phone/server.py
+++ src/phone/server.py
@@ -15,13 +15,16 @@ async def format_phone(request: Request) -> Response:
     number = await request.json()
     return Response(get_formatted_phone(number['phone']), media_type="text/html")
 
+
 @app.post("/unify_phone_from_form")
 async def format_phone_form(phone: str = Form()) -> Response:
     return Response(get_formatted_phone(phone), media_type="text/html")
 
+
 @app.get("/unify_phone_from_query")
 async def format_phone_query(phone: str) -> Response:
     return Response(get_formatted_phone(phone), media_type="text/html")
+
 
 @app.get("/unify_phone_from_cookies")
 async def format_phone_cookie(phone: str = Cookie()) -> Response:
